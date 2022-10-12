a64eb8a8c75af0c490997d17ccc1bdffb547f67c
Daiga G <daigagrieze@gmail.com>
2022-10-12 22:17:51 +0300

    new

---
+++ day2_2.py
@@ -0,0 +1,7 @@
+width = float(input("Enter the width of the room in meters, please:\n"))
+lenght = float(input("Enter the lenght of the room in meters, please:\n"))
+height = float(input("Enter the height of the room in meters, please:\n"))
+
+volume = width * lenght * height
+
+print(f"The volume of the room is {volume}!") 
