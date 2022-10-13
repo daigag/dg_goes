0919de59291ad7cef9bc85ff34bcfa5ca8ec6643
Daiga G <daigagrieze@gmail.com>
2022-10-13 15:37:20 +0300

    new

---
+++ day5_2.py
@@ -0,0 +1,11 @@
+# Write a program to recognize a text symbol
+# The user (first player) enters the text.
+# Only asterisks instead of letters are output.
+# Assume that there are no numbers, but there may be spaces.
+# The user (i.e. the other player) enters the symbol.
+# If the letter is found, then the letter is displayed in ALL the appropriate places, all other letters remain asterisks.
+# Example:
+# First input: Kartupeļu lauks -> ********* *****
+# Second input: a -> *a****** *a***
+
+txt = input("Enter your set word: ")
